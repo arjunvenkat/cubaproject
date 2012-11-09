4601744af9d5f21b50237614d297cd413e854fe2
Arjun Venkataswamy <avenkat2@gmail.com>
2012-11-08 23:12:17 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cubaproject2::Application.config.secret_token = 'c04ebb54eb67b0f104b489e5e1a37df10e5baabf3b6c1fdaa17b035dea1cb775d656a2d18fd0be00455bbb6447a8a47c697361cebf9918e15f1ff7c017382980'
