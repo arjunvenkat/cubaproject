725db2e34732b0e7eaecbdc27314c4296406f1ff
Arjun Venkataswamy <avenkat2@gmail.com>
2012-12-01 17:37:19 -0600

    adds spanish support on home page

--- vendor/extensions/interviews/app/models/refinery/interviews/interview.rb
+++ vendor/extensions/interviews/app/models/refinery/interviews/interview.rb
@@ -5,7 +5,7 @@ module Refinery
 
       attr_accessible :first_name, :last_name, :city, :date_of_entry, :description, :description_es, :picture_id, :position
 
-      acts_as_indexed :fields => [:first_name, :last_name, :city, :description]
+      acts_as_indexed :fields => [:first_name, :last_name, :city, :description, :description_es]
 
       belongs_to :picture, :class_name => '::Refinery::Image'
       has_many :clips, :class_name => '::Refinery::Clips::Clip', :dependent => :destroy
