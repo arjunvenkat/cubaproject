1f74430393cb80a38d58536f318b75d2dee02025
Arjun Venkataswamy <avenkat2@gmail.com>
2012-12-02 10:08:53 -0600

    fixes delete interview bug

--- vendor/extensions/interviews/app/controllers/refinery/interviews/admin/interviews_controller.rb
+++ vendor/extensions/interviews/app/controllers/refinery/interviews/admin/interviews_controller.rb
@@ -5,6 +5,17 @@ module Refinery
 
         crudify :'refinery/interviews/interview',
                 :title_attribute => 'first_name', :xhr_paging => true
+          def destroy
+            @interview = ::Refinery::Interviews::Interview.find(params['id'])
+
+            # raise @interview.inspect
+            if @interview.delete
+              flash.notice = t('destroyed', :scope => 'refinery.crudify', :what => "'#{@interview.first_name}'")
+              redirect_to '/refinery/interviews'
+            else
+              redirect_to '/refinery/interviews'
+            end
+          end
 
       end
     end
